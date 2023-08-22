d79c5ada0f2d0039258b386e55da335a7770225b
luisgustavorr <luisgustavo20061@gmail.com>
2023-08-22 15:32:14 -0300

    UPDATE

--- Models/post_receivers/insert_fechamento.php
+++ Models/post_receivers/insert_fechamento.php
@@ -1,7 +1,13 @@
 <?php 
 include('../../MySql.php');
 date_default_timezone_set('America/Sao_Paulo');
+try{
     $equip = \MySql::conectar()->prepare("INSERT INTO `tb_fechamento` (`id`, `dinheiro`, `cartao`, `moeda`, `pix`, `sangria`, `data`) VALUES (NULL, ?, ?, ?, ?, ?, ?)");
-    $equip->execute(array($_POST['dinheiro_informadas'],$_POST['cartao_informadas'],$_POST['moedas_informadas'],$_POST['pix_informadas'],$_POST['sangria_informadas'],date("Y-m-d")));
+    $equip->execute(array(str_replace(',','.',$_POST['dinheiro_informadas']),str_replace(',','.',$_POST['cartao_informadas']),str_replace(',','.',$_POST['moedas_informadas']),str_replace(',','.',$_POST['pix_informadas']),str_replace(',','.',$_POST['sangria_informadas']),date("Y-m-d")));
     $equip = $equip->fetch();
+}catch(Exception $e){
+    echo 'ERRO: Preencha todos os valores!';
+}
+
+
     ?>
