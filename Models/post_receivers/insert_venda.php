1934ecb876f7700e52467a138380d019535ca53c
luisgustavorr <luisgustavo20061@gmail.com>
2023-08-23 15:35:14 -0300

    Update

--- Models/post_receivers/insert_venda.php
+++ Models/post_receivers/insert_venda.php
@@ -16,9 +16,9 @@ if(!empty($colab)){
   setcookie("caixa", $colab['caixa'], time()+20*24*60*60);
   foreach ($_POST['produtos'] as $key => $value) {
     $produto = \MySql::conectar()->prepare("INSERT INTO `tb_vendas` (`id`, `colaborador`, `data`, `valor`, `caixa`,`produto`,`forma_pagamento`) VALUES (NULL, ?, ?, ?, ?,?,?); ");
-    $produto->execute(array($_POST['colaborador'],date("Y-m-d h:i:sa"),$value['preco']*$value['quantidade'],$_POST['caixa'],$value['id'],$_POST['pagamento']));
+    $produto->execute(array($_POST['colaborador'],date("Y-m-d h:i:sa"),$value['preco']*$value['quantidade'],trim($_POST['caixa']),$value['id'],$_POST['pagamento']));
     $atualizar_caixa = \MySql::conectar()->prepare("UPDATE `tb_caixas` SET `valor_atual` = `valor_atual` + ? WHERE `tb_caixas`.`caixa` = ? ");
-    $atualizar_caixa->execute(array($_POST['valor'],$_POST['caixa']));
+    $atualizar_caixa->execute(array($_POST['valor'],trim($_POST['caixa'])));
   
   }
 
