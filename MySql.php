a7a5b70d2bdf6efb8558ac7ccb37a2b7cc347c69
luisgustavorr <luisgustavo20061@gmail.com>
2023-08-28 14:25:14 -0300

    update

--- MySql.php
+++ MySql.php
@@ -7,7 +7,7 @@ class MySql{
 					// code...
 				
 				try{
-					self::$pdo = new PDO('mysql:host=sh-pro72.hostgator.com.br;dbname=urbana06_mixpdv.mixsalgados','urbana06_mixpdv','G4l01313',array(PDO ::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+					self::$pdo = new PDO('mysql:host=pro107.dnspro.com.br;dbname=spacemid_mixpvd.mixsalgados','spacemid_luis','G4l01313',array(PDO ::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 					self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 					}catch(Exception $e){
 						echo '<h2>Erro ao se conectar com o banco de dados!</h2>';
