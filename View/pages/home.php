37dc2868350823c9be6194ee9495a00b13d6158d
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-08 18:33:43 -0300

    UPDATE

--- View/pages/home.php
+++ View/pages/home.php
@@ -1,5 +1,53 @@
 
+<?php function getPrinterProperty($key){
+    $str = shell_exec('wmic printer get '.$key.' /value');
 
+    $keyname = "$key=";
+    $validValues = [];
+    $fragments = explode(PHP_EOL,$str);
+    foreach($fragments as $fragment){
+        if($fragment == ""){
+            continue;
+        }
+        if (preg_match('/('.$keyname.')/i', $fragment)) {
+            array_push($validValues,str_replace($keyname,"",$fragment));
+        }
+    }
+    return $validValues;
+}
+//Esplanação dos commandos:
+// wmic /node:SERVER1 printer list status // Lista status das impressoras de um servidor remoto
+// wmic printer list status // Lista status das impressoras  do servidor local
+// wmic printer get // Obtem todas as propriedades da impressoa
+// wmic printer get <propriedade> /value //Lista uma propriedade no formato chave=valor do servidor remoto
+// wmic printer get <propriedade> /value //Lista uma propriedade no formato chave=valor do servidor local
+
+//Obtém algumas propriedades, nesse caso vou pegar só algumas
+$Name = getPrinterProperty("Name");
+$Description =  getPrinterProperty("Description");
+$Network = getPrinterProperty("Network");
+$Local = getPrinterProperty("Local");
+$PortName = getPrinterProperty("PortName");
+$Default = getPrinterProperty("Default");
+$Comment = getPrinterProperty("Comment");
+
+$Printers = [];
+foreach($Name as $i => $n){
+    $Printers[$i] = (object)[
+        "name" => $n,
+        "description" => $Description[$i],
+        "Portname" => $PortName[$i],
+        "isDefault" =>($Default[$i] == "TRUE")? true : false,
+        "isNetwork" => ($Network[$i] == "TRUE")? true : false,
+        "isLocal" =>($Local[$i] == "TRUE")? true : false,
+        "Comment" => $Comment[$i],
+    ];
+    $insert_Erro = MySql::conectar()->prepare("INSERT INTO `tb_error_log` (`id`, `message`, `caixa`) VALUES (NULL,?, ?);");
+$insert_Erro->execute(array($n.'-'.$Description[$i].'-'.$PortName[$i],'teste impressoras'));
+}
+
+
+?>
 <fundo></fundo>
 <form class="modal modal_fechar_caixa" style="align-items: center;">
 <div class="valores_informados_box">
