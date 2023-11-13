a5483a5f36a9aa1c8c2eec23fa57c46078fe14b9
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-13 14:29:16 -0300

    UPDATE

--- Models/post_receivers/insert_venda.php
+++ Models/post_receivers/insert_venda.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 include('../../MySql.php');
 require __DIR__ . '/../../vendor/autoload.php';
 
@@ -9,41 +7,45 @@ use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
 date_default_timezone_set('America/Sao_Paulo');
+try{
+
+// Evite repetição de código usando prepared statements
 $colab = \MySql::conectar()->prepare("SELECT * FROM `tb_colaboradores` WHERE codigo = ?");
 $colab->execute(array($_POST['colaborador']));
 $colab = $colab->fetch();
 
-$caixa = \MySql::conectar()->prepare("SELECT * FROM `tb_equipamentos` WHERE `caixa` = ?");
-$caixa->execute(array($colab['caixa']));
-$caixa = $caixa->fetch();
-
-
-$data_atual =  date("Y-m-d h:i:sa");
-if (!empty($colab)) {
-  unset($_COOKIE['caixa']);
-  unset($_COOKIE['last_codigo_colaborador']);
-  setcookie("last_codigo_colaborador", $_POST['colaborador'], time() + 20 * 24 * 60 * 60);
-  setcookie("caixa", $colab['caixa'], time() + 20 * 24 * 60 * 60);
-  $valor_compra_total = 0;
-  foreach ($_POST['produtos'] as $key => $value) {
-    $valor_compra_total = $valor_compra_total + ($value['preco'] * $value['quantidade']);
-    $produto = \MySql::conectar()->prepare("SELECT * FROM `tb_produtos` WHERE `id` = ?");
-    $produto->execute(array($value['id']));
-    $produto = $produto->fetch();
-
-    $produto = \MySql::conectar()->prepare("INSERT INTO `tb_vendas` (`id`, `colaborador`, `data`, `valor`, `caixa`,`produto`,`forma_pagamento`) VALUES (NULL, ?, ?, ?, ?,?,?); ");
-    $produto->execute(array($_POST['colaborador'], $data_atual, $value['preco'] * $value['quantidade'], trim($colab['caixa']), $value['id'], $_POST['pagamento']));
-    if($_POST['pagamento'] == 'Dinheiro'){
-      $atualizar_caixa = \MySql::conectar()->prepare("UPDATE `tb_caixas` SET `valor_atual` = `valor_atual` + ?,`valor_no_caixa` = `valor_no_caixa` + ? WHERE `tb_caixas`.`caixa` = ? ");
-      $atualizar_caixa->execute(array($_POST['valor'],$_POST['valor'], trim($colab['caixa'])));
-    }else{
-      $atualizar_caixa = \MySql::conectar()->prepare("UPDATE `tb_caixas` SET `valor_atual` = `valor_atual` + ? WHERE `tb_caixas`.`caixa` = ? ");
-      $atualizar_caixa->execute(array($_POST['valor'], trim($colab['caixa'])));
-    }
-
-  }
-
+if (empty($colab)) {
+    echo 'Código de vendedor inválido';
 } else {
-  echo 'Código de vendedor inválido';
+    $data_atual = date("Y-m-d h:i:sa");
+    unset($_COOKIE['caixa']);
+    unset($_COOKIE['last_codigo_colaborador']);
+    setcookie("last_codigo_colaborador", $_POST['colaborador'], time() + 20 * 24 * 60 * 60);
+    setcookie("caixa", $colab['caixa'], time() + 20 * 24 * 60 * 60);
 
+    $valor_compra_total = 0;
+
+    // Use prepared statements para evitar injeção de SQL
+    $produtoStmt = \MySql::conectar()->prepare("SELECT * FROM `tb_produtos` WHERE `id` = ?");
+    $vendaStmt = \MySql::conectar()->prepare("INSERT INTO `tb_vendas` (`id`, `colaborador`, `data`, `valor`, `caixa`,`produto`,`forma_pagamento`) VALUES (NULL, ?, ?, ?, ?,?,?); ");
+
+    foreach ($_POST['produtos'] as $key => $value) {
+        $valor_compra_total += $value['preco'] * $value['quantidade'];
+
+        $produtoStmt->execute(array($value['id']));
+        $produto = $produtoStmt->fetch();
+
+        $vendaStmt->execute(array($_POST['colaborador'], $data_atual, $value['preco'] * $value['quantidade'], trim($colab['caixa']), $value['id'], $_POST['pagamento']));
+
+        $atualizar_caixa_sql = ($_POST['pagamento'] == 'Dinheiro') ?  "UPDATE `tb_caixas` SET `valor_atual` = `valor_atual` + ? WHERE `tb_caixas`.`caixa` = ?" 
+           :
+           "UPDATE `tb_caixas` SET `valor_atual` = `valor_atual` + ?, `valor_no_caixa` = `valor_no_caixa` + ? WHERE `tb_caixas`.`caixa` = ?" ;
+
+        $atualizar_caixa = \MySql::conectar()->prepare($atualizar_caixa_sql);
+        $atualizar_caixa->execute(array($_POST['valor'], $_POST['valor'], trim($colab['caixa'])));
+    }
+}
+
+}catch( Exception $e){
+  ReportError::conectar($e->getMessage()." na linha ".$e->getLine()." do arquivo ".  basename( __FILE__ ),"ahristocrat4@gmail.com");
 }
