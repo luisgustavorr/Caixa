f2358f6cae43e88b7c59442e8cbe0989c4ace2b0
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-20 15:23:51 -0300

    UPDATE

--- Models/post_receivers/delete_last_venda.php
+++ Models/post_receivers/delete_last_venda.php
@@ -4,5 +4,6 @@ include('../../MySql.php');
     $equip = \MySql::conectar()->prepare("DELETE FROM `tb_vendas` WHERE `pedido_id` = 0 AND `caixa` = ?
     ORDER BY `tb_vendas`.`id` DESC
     LIMIT 1;");
-    $equip->execute(array($_COOKIE["last_codigo_colaborador"]));
+    $equip->execute(array($_COOKIE["caixa"]));
+    echo'aaaa'.$_COOKIE["caixa"];
 ?>
