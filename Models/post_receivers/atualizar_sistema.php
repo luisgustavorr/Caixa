b6a3bdf80cdf11c1a8b2db91c0a998bc0630f51a
luisgustavorr <luisgustavo20061@gmail.com>
2023-11-24 15:49:38 -0300

    update

--- Models/post_receivers/atualizar_sistema.php
+++ Models/post_receivers/atualizar_sistema.php
@@ -1,21 +1,5 @@
 <?php
-
-// Executa o comando 'git fetch --all' para buscar atualizações do repositório remoto
-shell_exec('git fetch --all');
-
-// Obtém a hash do commit local
-$localCommitHash = trim(shell_exec('git rev-parse HEAD'));
-
-// Obtém a hash do commit remoto
-$remoteCommitHash = trim(shell_exec('git rev-parse origin/main'));
-
-// Compara as hashes para verificar se o código está atualizado
-if ($localCommitHash === $remoteCommitHash) {
-} else {
-    // Executa 'git reset --hard origin/main' para atualizar o código local
-    shell_exec('git reset --hard origin/main');
-
-    echo "O código foi atualizado para a versão mais recente.\n";
-}
+exec('git fetch --all');
+exec('git reset --hard origin/main')
 ?>
 
