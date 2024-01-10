9cfb6a8b3daeb033a43ef46b5deb950ed53e8932
Luís Gustavo <luisgustavo20061@gmail.com>
2024-01-10 13:44:10 -0300

    UPDATE

--- MySql.php
+++ MySql.php
@@ -14,8 +14,8 @@ class MySql
 				self::$pdo = new PDO('mysql:host=pro107.dnspro.com.br;dbname=spacemid_mixpvd.mixsalgados', 'spacemid_luis', 'G4l01313', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch (Exception $e) {
-				echo '<h2>Erro ao se conectar com o banco de dados!</h2>';
-				echo $e;
+				echo '<h2>Erro ao se conectar com o banco de dados! Reinicie o sistema, e caso o erro persista entre em contato conosco.</h2>';
+				// echo $e;
 			}
 		}
 		return self::$pdo;
--- js/procurar_pedidos_proximos.js
+++ js/procurar_pedidos_proximos.js
@@ -11,6 +11,42 @@ function scanPedidosProximos() {
             });
         }
      })
+     $.post(
+        "Models/post_receivers/select_pedidos.php",
+        { anytime: true },
+        function (ret) {
+          let pedidos = JSON.parse(ret);
+          if (!$.isEmptyObject(pedidos)) {
+            pedidos.forEach((element) => {
+              let data = element.data_entrega.split(" ");
+              $(".lista_pedidos").append(
+                "<span > <input class='pedido_feito' type='checkbox' pedido='" +
+                  element.id +
+                  "'><label onclick='editarPedido(this)' pedido='" +
+                  JSON.stringify(element) +
+                  "'>" +
+                  element.cliente +
+                  "-" +
+                  element.data_entrega +
+                  " </label></span>"
+              );
+              $(".pedido_feito").change(function () {
+                let pedido = $(this).attr("pedido");
+                data = {
+                  pedido: pedido,
+                };
+                $.post(
+                  "Models/post_receivers/update_pedido_feito.php",
+                  data,
+                  function (ret) {
+                    location.reload();
+                  }
+                );
+              });
+            });
+          }
+        }
+      );
 }
 
 // Chame a função pela primeira vez (opcional, caso queira executá-la imediatamente)
